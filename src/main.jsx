23a9399589452a1f30d13d85ddeadef9cae5274c
Mehebul02 <mehebul2123@gmail.com>
2024-06-22 02:12:55 +0600

    Type text added

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import router from './Routes/Routes.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={router} >
+    </RouterProvider>
+      <App/>
   </React.StrictMode>,
 )
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,21 +1,51 @@
 import Container from "../../shared/Container/Container";
 import banner from '../../../assets/images/banner/banner_2.png'
+import { TypeAnimation } from "react-type-animation";
 const Banner = () => {
     return (
        
            
-            <div className="grid grid-cols-6 bg-[#ECF0F3] ">
+            <div className=" bg-[#ECF0F3] mt-10">
                
-                 {/* images  */}
+               <Container>
+               <div className="grid grid-cols-6">
+                     {/* images  */}
                  <div className="col-span-2 p-10 rounded-lg">
                     <div>
-                        <img className="bg-[#2a3c45] p-2 border-[10px] border-white shadow-2xl rounded-lg" src={banner} alt="" />
+                        <img className="bg-[#2a3c45]  p-2 border-[10px] border-white shadow-2xl rounded-tl-3xl rounded-br-3xl" src={banner} alt="" />
                         </div> 
                 </div>
                 {/* content  */}
-                <div className="col-span-4">
-                    content 
+                <div className="col-span-4 p-20">
+                 <div className="space-y-4">
+                 <h5 className="text-xl font-poppins font-semibold">WELCOME TO MY WORLD</h5> 
+                   <h1 className="text-6xl text-[#25282C] font-montserrat font-bold">Hi, I’m <span className="text-[#FE135C]">Mehebul Alif</span> </h1><br />
+                   <span className="text-5xl font-montserrat font-semibold"> <span 
+                   className="">
+                   <span className="text-[#FE135C]">a</span>
+                    <TypeAnimation
+  sequence={[
+  
+    500,
+    ' web', //  Continuing previous Text
+    500,
+    ' web developer',
+    500,
+    ' web',
+    500,
+    
+    
+    '',
+    500,
+  ]}
+  style={{ fontSize: '' }}
+  repeat={Infinity}
+/>
+                    </span> </span>
+                 </div>
                 </div>
+                </div>
+               </Container>
               
             </div>
        
