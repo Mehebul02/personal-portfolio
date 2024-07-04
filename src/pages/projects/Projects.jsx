0d0de177b45dfa509a5dffa8aa7744b8e0712c08
Mehebul02 <mehebul2123@gmail.com>
2024-07-04 13:32:00 +0600

    Project card section added

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -3,17 +3,25 @@ import ProjectsCard from "./ProjectsCard";
 const Projects = () => {
     const projects = [
         {
-          title: "Project One",
-          description: "This is a brief description of project one.",
-          imageUrl: "https://via.placeholder.com/300x200",
+          id: 1,
+          title: "Pet Adoption ",
+          description: "Manage pet adoptions effectively with our streamlined admin dashboard, featuring comprehensive oversight, adoption tracking, user management, analytics, communication tools, customization, security, and continuous improvement.",
+          imageUrl: "https://i.ibb.co/42MbF7c/pet-adeption.png",
           projectLink: "https://pet-adoption-ed896.web.app",
+          technologies: ["React", "Firebase", "Tailwind CSS" ,"Node js","Mongodb"],
+          date: "2023-06-15",
+          
         },
         {
-          title: "Project Two",
-          description: "This is a brief description of project two.",
-          imageUrl: "https://via.placeholder.com/300x200",
-          projectLink: "https://example.com/project-two",
-        },
+            id: 2,
+            title: "Project One",
+            description: "This full-stack application project features a comprehensive dashboard designed for efficient management and user interaction. The dashboard serves as the central hub, providing administrators and users with essential tools and insights for streamlined operations.",
+            imageUrl: "https://i.ibb.co/McrV59L/case-study.png",
+            projectLink: "https://pet-adoption-ed896.web.app",
+            technologies: ["React", "Firebase", "Tailwind CSS" ,"Node js","Mongodb"],
+            date: "2023-06-15",
+      
+        }
         // Add more projects here
       ];
     return (
@@ -29,16 +37,19 @@ const Projects = () => {
         <div className="divider"></div>
         </div>
         {/* card projects  */}
-        <div className="container mx-auto p-4">
-      <h1 className="text-4xl font-bold mb-8 text-center">My Projects</h1>
+        <div className="container mx-auto p-4 lg:mt-10">
+      {/* <h1 className="text-4xl font-bold mb-8 text-center">My Projects</h1> */}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {projects.map((project, index) => (
+        {projects.map((project) => (
           <ProjectsCard
-            key={index}
+            key={project.id}
             title={project.title}
             description={project.description}
             imageUrl={project.imageUrl}
             projectLink={project.projectLink}
+            technologies={project.technologies}
+            date={project.date}
+            status={project.status}
           />
         ))}
       </div>
--- src/pages/projects/ProjectsCard.jsx
+++ src/pages/projects/ProjectsCard.jsx
@@ -1,23 +1,49 @@
+import { useState } from "react";
 
-const ProjectsCard = ({ title, description, imageUrl, projectLink }) => {
+const ProjectsCard = ({  title, description, imageUrl, projectLink, technologies, date  }) => {
+    const [expanded, setExpanded] = useState(false);
+
+    const toggleExpanded = () => {
+      setExpanded(!expanded);
+    };
     return (
-        <div>
-           <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
-      <img className="w-full" src={imageUrl} alt={title} />
-      <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{title}</div>
-        <p className="text-gray-700 text-base">{description}</p>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white transform transition duration-500 hover:scale-105">
+      <img className="w-full h-62 p-6 object-cover" src={imageUrl} alt={title} />
+      <div className="px-6 py-4 space-y-2">
+        <h3 className="font-bold text-xl font-montserrat text-gray-900">{title}</h3>
+        <div className="flex flex-wrap">
+        <p className="text-gray-700 mb-2">{expanded ? description : `${description.substring(0, 36)}...`}</p>
+        {description.length > 100 && (
+          <button
+            onClick={toggleExpanded}
+            className="text-blue-500 text-sm hover:underline focus:outline-none"
+          >
+            {expanded ? "See Less" : "See More"}
+          </button>
+        )}
+        </div>
+        <div className="">
+          <div className="flex flex-wrap  items-center">
+          <h4 className="font-bold text-gray-900 font-montserrat">Technologies: </h4>
+            {technologies.map((tech, index) => (
+              <p key={index} className="text-gray-700 font-poppins text-sm"> {tech} ,</p>
+            ))}
+          </div>
+        </div>
+        <div className="">
+          <p className="text-gray-700"><strong>Date:</strong> {date}</p>
+         
+        </div>
       </div>
-      <div className="px-6 pt-4 pb-2">
+      <div className="px-6 pb-4 pt-4 flex justify-end">
         <a
           href={projectLink}
-          className="inline-block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          className="bg-[#FE135C] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
           View Project
         </a>
       </div>
-    </div> 
-        </div>
+    </div>
     );
 };
 
