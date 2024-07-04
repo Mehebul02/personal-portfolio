edcfefdf2e4d08f2d176b704ab5338468f3b722f
Mehebul02 <mehebul2123@gmail.com>
2024-07-04 16:51:00 +0600

    footer section added

--- src/pages/Home/footer/Footer.jsx
+++ src/pages/Home/footer/Footer.jsx
@@ -1,9 +1,56 @@
-
+import { FaFacebook, FaLinkedin } from 'react-icons/fa';
+import logo from '../../../assets/images/logo/logo.png'
+import { BsTwitterX } from 'react-icons/bs';
 const Footer = () => {
     return (
-        <div className="text-center mt-20">
-            <h1>Copyright © 2024 - All right reserved by ACME Industries Ltd</h1>
-        </div>
+       <div>
+        <div className="divider shadow-2xl"></div>
+         <footer className="footer  text-base-content p-10 my-5 lg:my-14">
+            
+            <aside>
+              <img className='w-32' src={logo} alt="" />
+              <div className="space-y-6 mt-3">
+      {/* <h1 className="text-xl uppercase underline font-montserrat font-semibold ">Find With me</h1> */}
+      <div className="flex flex-wrap space-x-10 " >
+      
+      <a href='https://web.facebook.com/alif.islam.212?_rdc=1&_rdr' target="_blank">
+      <h1 className="bg-gray-700 p-2 title rounded-tl-lg rounded-br-lg hover:bg-gradient-to-t from-rose-500 to-red-500 hover:border-b-2 border-white">
+      <FaFacebook className="text-white text-2xl" />
+    </h1>
+      </a>
+     
+       <a href='https://www.linkedin.com/in/mehebul-alif-9b865025b' target="_blank">
+       <h1 className="bg-gray-700 p-2 rounded-tl-lg rounded-br-lg hover:bg-gradient-to-t from-rose-500 to-red-500"><FaLinkedin  className="text-white text-2xl"/> </h1>
+       </a>
+       
+        <h1 className="bg-gray-700 p-2 rounded-tl-lg rounded-br-lg hover:bg-gradient-to-t from-rose-500 to-red-500"><BsTwitterX className="text-white text-2xl"/></h1>
+      </div>
+    </div>
+            </aside>
+            <nav>
+              <h6 className="text-xl text-[#FE135C] font-montserrat font-semibold ">
+              QUICK LINK</h6>
+              <a className="link link-hover font-montserrat font-medium">Home</a>
+              <a className="link link-hover font-montserrat font-medium">About</a>
+              <a className="link link-hover font-montserrat font-medium">Skill</a>
+              <a className="link link-hover font-montserrat font-medium">Contact</a>
+             
+            </nav>
+            <nav>
+              <h6 className="footer-title">Company</h6>
+              <a className="link link-hover">About us</a>
+              <a className="link link-hover">Contact</a>
+              <a className="link link-hover">Jobs</a>
+              <a className="link link-hover">Press kit</a>
+            </nav>
+            <nav>
+              <h6 className="footer-title">Legal</h6>
+              <a className="link link-hover">Terms of use</a>
+              <a className="link link-hover">Privacy policy</a>
+              <a className="link link-hover">Cookie policy</a>
+            </nav>
+          </footer>
+       </div>
     );
 };
 
